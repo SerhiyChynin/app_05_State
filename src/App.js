d90e46d219e2e263a295d885fb1f2b2d06d45cbc
SerhiiChynin <sergey.chinin@gmail.com>
2022-12-20 14:35:56 +0200

    State

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Comp1 from './Comp1';
+import Comp2 from './Comp2';
+
+
+function App() {
+
+  return (
+    <div>
+      <Comp1></Comp1>
+      <hr />
+      <Comp2></Comp2>
+    </div>
+  );
+}
+
+export default App;
